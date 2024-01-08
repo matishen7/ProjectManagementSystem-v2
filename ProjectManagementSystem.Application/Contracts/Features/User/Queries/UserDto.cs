f54309d477297c7b8a2049684f0c6e0e13b6f8d6
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2024-01-08 14:49:27 -0800

    add UserDto

---
+++ ProjectManagementSystem.Application/Contracts/Features/User/Queries/UserDto.cs
@@ -0,0 +1,18 @@
+﻿using ProjectManagementSystem.Application.Contracts.Features.Project.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem.Application.Contracts.Features.User.Queries
+{
+    public class UserDto
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public ICollection<ProjectDto> AssignedProjects { get; set; }
+    }
+}
