d5a54b64edd2e4d669928ba28bc6b54e84bb4ca8
Muratbek Ishenbaev <matishen7@gmail.com>
2024-01-08 14:45:55 -0800

    add TaskDto and ProjectDto

---
+++ ProjectManagementSystem.Application/Contracts/Features/Project/Queries/ProjectDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem.Application.Contracts.Features.Project.Queries
+{
+    public class ProjectDto
+    {
+        public int ProjectId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
