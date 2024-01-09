25b7bf712afb41671857c6465371f43a3e05043d
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2024-01-09 09:42:19 -0800

    add GetUserQueryHandler

---
+++ ProjectManagementSystem.Application/Contracts/Features/User/Queries/GetUserQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem.Application.Contracts.Features.User.Queries
+{
+    public class GetUserQuery : IRequest<UserDto>
+    {
+        public int UserId { get; set; }
+    }
+}
---
+++ ProjectManagementSystem.Application/Contracts/Features/User/Queries/GetUserQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using ProjectManagementSystem.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem.Application.Contracts.Features.User.Queries
+{
+    public class GetUserQueryHandler : IRequestHandler<GetUserQuery, UserDto>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public GetUserQueryHandler(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<UserDto> Handle(GetUserQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.UserId);
+            return _mapper.Map<UserDto>(user);
+        }
+    }
+}
