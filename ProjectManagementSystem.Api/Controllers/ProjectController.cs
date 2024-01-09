6d5567ab4dc37b467c3cc59231a9dc7a130c6fd7
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2024-01-09 09:21:08 -0800

    Add ProjectController

---
+++ ProjectManagementSystem.Api/Controllers/ProjectController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManagementSystem.Application.Contracts.Features.Project.Queries;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProjectManagementSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProjectController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{projectId}")]
+        public async Task<IActionResult> GetProject(int projectId)
+        {
+            var project = await _mediator.Send(new GetProjectQuery { ProjectId = projectId });
+            return Ok(project);
+        }
+    }
+}
