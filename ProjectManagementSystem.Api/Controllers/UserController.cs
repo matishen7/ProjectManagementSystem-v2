885eb3ed80a930445d1d3eb343e6fe040952dcd6
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2024-01-09 09:43:54 -0800

    add UserController

---
+++ ProjectManagementSystem.Api/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManagementSystem.Application.Contracts.Features.Project.Queries;
+using ProjectManagementSystem.Application.Contracts.Features.User.Queries;
+
+namespace ProjectManagementSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetUser(int userId)
+        {
+            var user = await _mediator.Send(new GetUserQuery { UserId = userId });
+            return Ok(user);
+        }
+    }
+}
