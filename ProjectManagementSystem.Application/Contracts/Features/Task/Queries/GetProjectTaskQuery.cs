060f12038e6e43aa4c7edba8c4af2ff0b0cc0c2a
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2024-01-09 09:12:52 -0800

    add IProjectTaskRepository and IProjectTaskRepository

---
+++ ProjectManagementSystem.Application/Contracts/Features/Task/Queries/GetProjectTaskQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem.Application.Contracts.Features.Task.Queries
+{
+    public class GetProjectTaskQuery : IRequest<ProjectTaskDto>
+    {
+        public int TaskId { get; set; }
+    }
+}
---
+++ ProjectManagementSystem.Application/Contracts/Features/Task/Queries/GetProjectTaskQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using ProjectManagementSystem.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem.Application.Contracts.Features.Task.Queries
+{
+    public class GetProjectTaskQueryHandler : IRequestHandler<GetProjectTaskQuery, ProjectTaskDto>
+    {
+        private readonly IProjectTaskRepository _projectTaskRepository;
+        private readonly IMapper _mapper;
+
+        public GetProjectTaskQueryHandler(IProjectTaskRepository projectTaskRepository, IMapper mapper)
+        {
+            _projectTaskRepository = projectTaskRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ProjectTaskDto> Handle(GetProjectTaskQuery request, CancellationToken cancellationToken)
+        {
+            var task = await _projectTaskRepository.GetByIdAsync(request.TaskId);
+            return _mapper.Map<ProjectTaskDto>(task);
+        }
+    }
+}
