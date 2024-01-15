e89158269829303f3977498e641ba93f1b018351
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2024-01-15 14:06:53 -0800

    implement UserRepository

---
+++ ProjectManagementSystem.Persistance/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectManagementSystem.Application.Contracts.Persistence;
+using ProjectManagementSystem.Core.Entities;
+using ProjectManagementSystem.Persistance.DatabaseContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem.Persistance.Repositories
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        public UserRepository(ProjectManagementDbContext context) : base(context)
+        {
+        }
+    }
+}
